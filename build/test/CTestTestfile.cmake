d48c7c29e8d8e7f4decdca1ea70f2469268b7f62
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2025-11-01 13:29:24 +0100

    removed the build folder

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nik/Apps/PyFi/test
-# Build directory: /Users/nik/Apps/PyFi/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/nik/Apps/PyFi/build/test/test_bond-b12d07c_include.cmake")
--- build/test/test_bond-b12d07c_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( [==[unit.Test vector length]==] /Users/nik/Apps/PyFi/build/test/test_bond [==[Test vector length]==]  )
-set_tests_properties( [==[unit.Test vector length]==] PROPERTIES WORKING_DIRECTORY /Users/nik/Apps/PyFi/build/test)
-set( test_bond_TESTS [==[unit.Test vector length]==])
